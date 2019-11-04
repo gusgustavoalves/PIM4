fcb79ee9f28122fdd378773aaddf10293b45052e
willgualberto <willgualberto@live.com>
2019-11-04 11:07:02 -0300

    passando alterações do dia 03/11 para a linha do tempo Master.

---
+++ View/Coleta/Consultar_Coleta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PIM.View.Coleta
+{
+    public partial class frmConsultar_Coleta : Form
+    {
+        public frmConsultar_Coleta()
+        {
+            InitializeComponent();
+        }
+    }
+}
