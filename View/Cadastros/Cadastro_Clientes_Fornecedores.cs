177c04540e259c1923b727efbdbcb36c61dba2bf
willgualberto <willgualberto@live.com>
2019-11-24 10:21:10 -0300

    criação da tela de clientes/ fornecedores.

--- View/Cadastros/Cadastro_Clientes_Fornecedores.cs
+++ View/Cadastros/Cadastro_Clientes_Fornecedores.cs
@@ -16,5 +16,20 @@ namespace PIM.View.Cadastros
         {
             InitializeComponent();
         }
+
+        private void bunifuShadowPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void bunifuTextBox14_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bunifuCustomLabel16_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
